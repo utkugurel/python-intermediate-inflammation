b79d97de1704684b1db9d840283f99b4195c93dd
Utku Gürel <utkugurel@gmail.com>
2023-12-11 11:46:40 +0100

    updated docstring

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,27 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: 2D array of inflammation
+    :return: 1D array of average values for each day
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: 2D array of inflammation
+    :return: 1D array of maximum values for each day
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
-    return np.min(data, axis=0)
+    """Calculate the daily min of a 2D inflammation data array.
 
+    :param data: 2D array of inflammation
+    :return: 1D array of minimum values for each day
+    """
+    return np.min(data, axis=0)
